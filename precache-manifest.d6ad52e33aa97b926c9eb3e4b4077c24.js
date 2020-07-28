3e8e41464fe7f00814eb4f6ee04b7e20b9ec9e44
Tijana <Tijana>
2020-07-28 16:09:24 +0200

    Updates

---
+++ precache-manifest.d6ad52e33aa97b926c9eb3e4b4077c24.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "707f6d35c5d768ebd087ff87b93b9646",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "4d7fb5705c66e962a86b",
+    "url": "/shop/static/css/2.2623327d.chunk.css"
+  },
+  {
+    "revision": "fa3b882aed341a889c79",
+    "url": "/shop/static/css/main.724631c2.chunk.css"
+  },
+  {
+    "revision": "4d7fb5705c66e962a86b",
+    "url": "/shop/static/js/2.0cd094e8.chunk.js"
+  },
+  {
+    "revision": "3b2a5349437879cfa70a1fc5f8fb9c29",
+    "url": "/shop/static/js/2.0cd094e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa3b882aed341a889c79",
+    "url": "/shop/static/js/main.cd8ad1cd.chunk.js"
+  },
+  {
+    "revision": "f255667955cb890e3272",
+    "url": "/shop/static/js/runtime-main.5d65f167.js"
+  },
+  {
+    "revision": "99ddaf19fe3e793cb63cb288159deae6",
+    "url": "/shop/static/media/pants.99ddaf19.jpg"
+  },
+  {
+    "revision": "eda47ffe9db94fc5311f2b285b7a0a3b",
+    "url": "/shop/static/media/shert.eda47ffe.jpg"
+  },
+  {
+    "revision": "6489579492115cd3c55c62ecc86191fd",
+    "url": "/shop/static/media/skirt.64895794.jpg"
+  }
+]);
